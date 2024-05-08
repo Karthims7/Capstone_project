82c7e4b42c38bf171a09460550c8b3be3f9d34a7
Ubuntu <ubuntu@ip-172-31-60-205.ec2.internal>
2024-05-08 12:23:27 +0000

    pushed

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
+echo "Logging into Docker Hub..."
 docker login -u karthims -p dckr_pat_M6JLQV_yR9w9tZoGYCixNmL83KA
-# Tag the image with the Docker Hub repository name and version
-if [[ $GIT_BRANCH == 'origin/main' ]]; then
-docker tag projectguvi:v1 karthims/prod
-docker push karthims/prod
-# Docker login
-elif [[ $GIT_BRANCH == 'origin/dev' ]]; then
-docker tag projectguvi:v1 karthims/dev
-docker push karthims/dev
+
+# Get the current branch name
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Current branch: $GIT_BRANCH"
+
+# Tag and push the image based on the branch
+if [[ $GIT_BRANCH == 'main' ]]; then
+    echo "Tagging and pushing image for main branch..."
+    docker tag projectguvi:v1 karthims/prod
+    docker push karthims/prod
+    echo "Image tagged and pushed to karthims/prod."
+elif [[ $GIT_BRANCH == 'dev' ]]; then
+    echo "Tagging and pushing image for dev branch..."
+    docker tag projectguvi:v1 karthims/dev
+    docker push karthims/dev
+    echo "Image tagged and pushed to karthims/dev."
+else
+    echo "No action taken. Branch not recognized."
 fi
+
